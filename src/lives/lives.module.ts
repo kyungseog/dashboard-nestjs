3a1c85819888c2d2138f3bb865f4eb787ade93c1
kyungseogkim <86609170+kyungseog@users.noreply.github.com>
2022-12-12 00:14:13 +0900

    Delete lives.module.ts

--- src/lives/lives.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LivesController } from './lives.controller';
-import { LivesService } from './lives.service';
-
-@Module({
-  controllers: [LivesController],
-  providers: [LivesService]
-})
-export class LivesModule {}
