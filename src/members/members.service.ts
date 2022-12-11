12b79d3c04ed74a8ff827e947d00393ab6aadc13
kyungseogkim <86609170+kyungseog@users.noreply.github.com>
2022-12-12 00:23:47 +0900

    Create members.service.ts

---
+++ src/members/members.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMemberDto } from './Dto/create-member.dto';
+import { Member } from './member.model';
+
+@Injectable()
+export class MembersService {
+  private members: Member[] = [];
+
+  getAllMembers(): Member[] {
+    return this.members;
+  }
+
+  createMember(createMemberDto: CreateMemberDto) {
+    const { id, partname, position, name, introduction } = createMemberDto;
+    const member: Member = {
+      id,
+      partname,
+      position,
+      name,
+      introduction,
+    };
+    this.members.push(member);
+    return this.members;
+  }
+
+  getMemberById(id: string): Member {
+    return this.members.find((member) => member.id === id);
+  }
+
+  updateMemberById(id: string, introduction: string): Member {
+    const member = this.getMemberById(id);
+    member.introduction = introduction;
+    return member;
+  }
+}
