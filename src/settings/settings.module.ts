3351476ac7ea8051c32d7843d682ed9aad0d3a9e
kyungseog kim <ks.kevin.kim@gmail.com>
2023-05-22 20:15:44 +0900

    create settings add

--- src/settings/settings.module.ts
+++ src/settings/settings.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SettingsController } from './settings.controller';
 import { SettingsService } from './settings.service';
+import { KoreaOrders } from 'src/entities/korea-orders.entity';
+import { Brands } from 'src/entities/brands.entity';
+import { Suppliers } from 'src/entities/suppliers.entity';
+import { Products } from 'src/entities/products.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([KoreaOrders, Brands, Suppliers])],
   controllers: [SettingsController],
-  providers: [SettingsService]
+  providers: [SettingsService],
 })
 export class SettingsModule {}
